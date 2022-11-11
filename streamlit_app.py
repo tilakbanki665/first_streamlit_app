9cb7ab8bb3d3da66cb44959b83f4d33537d318b8
Tilak Banki <117975408+tilakbanki665@users.noreply.github.com>
2022-11-11 05:13:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal Kale, Spinach and Rocket Smoothie Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
