4bfda54354490849285749104d7960fe46ec1010
Tilak Banki <117975408+tilakbanki665@users.noreply.github.com>
2022-11-15 01:33:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,5 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','kiwi')
 streamlit.write('The user entered', add_my_fruit)
-my_cur1 = my_cnx.cursor()
-my_cur1.execute("Select * from fruit_load_list")
-my_data_roww = my_cur1.fetchall()
-streamlit.dataframe(my_data_roww)
+import requests
+my_data_rows_response = requests.get("https://fruitvice.com/api/fruit/" + add_my_fruit)
