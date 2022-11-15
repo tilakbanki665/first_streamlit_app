307ed7a902206b0132207bee304883b394218e56
Tilak Banki <117975408+tilakbanki665@users.noreply.github.com>
2022-11-15 01:24:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','kiwi')
 streamlit.write('The user entered', add_my_fruit)
 import requests
-my_data_rows_response = requests.get(my_data_rows[add_my_fruit])
+my_data_rows_response = requests.get(my_data_rows.add_my_fruit)
